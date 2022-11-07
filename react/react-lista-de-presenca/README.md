c169b7efedd865278a8629412ad48fe58bcecd54
Bianca Viana <bianca.albuquerquevn@gmail.com>
2022-11-07 17:50:30 -0300

    commit inicial

---
+++ react/react-lista-de-presenca/README.md
@@ -0,0 +1,23 @@
+# Lista de presença com React.
+
+Este projeto é uma de lista de presença do curso Discover da [Rocketseat](https://github.com/Rocketseat), no qual eu aplico alguns dos conceitos fundamentais do ReactJs.
+
+## 🎯 Objetivo:
+
+Ter uma lista de presença que ao clicar no botão de adicionar, o nome da pessoa e a hora da chamada seja adicionada a lista e que esse informação apareça na tela.
+
+### 📁 Estrutura de pastas/arquivos:
+
+Na pasta pages ficarão todas as páginas da aplicação. Home será a pagina principal.
+Na pasta components ficarão os componentes que podem ser compartilhados ao longo de toda a aplicação.
+
+### 📚 Conhecimentos aplicados em:
+
+- Criação e reutilização de componentes;
+- Uso de props(propriedades) para tornar os componentes dinâmicos;
+- Hooks;
+- Estados (utilizando o useState);
+- useEffects;
+- Imutabilidade;
+- key Props;
+- Consumo de API;
---
+++ react/react-lista-de-presenca/src/pages/Home/index.jsx
@@ -0,0 +1,88 @@
+import './style.css';
+import Card from '../../components/Card';
+import React, { useState, useEffect } from 'react';
+
+//function component
+function Home() {
+
+  //pegar o nome de cada estudante
+  const [studentName, setStudentName] = useState();
+
+  //armazenar os estudantes na lista de presença.
+  const [students, setStudents] = useState([]);
+
+  //pegando o nome e avatar usando uma API
+  const [user, setUser] = useState({
+    name: '',
+    avatar: ''
+  });
+
+  function handleAddStudent() {
+
+    const input = document.querySelector('input');
+
+    if(input.value != ''){
+      const newStudent = {
+            name: studentName,
+            time: new Date().toLocaleTimeString('pt-br', {
+              hour: '2-digit',
+              minute: '2-digit',
+              second: '2-digit'
+            })
+          };
+
+          //Adicionando todos os estudantes na lista (o que tinha antes + o atual)
+          setStudents(prevState => [...prevState, newStudent]);
+    }
+   
+    input.value = '';
+  }
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/biancaaviana')
+    .then(response => response.json())
+    .then(data => {
+        setUser({
+          name: data.name,
+          avatar: data.avatar_url
+        })
+      })
+  }, [students])
+
+  return (
+    <div className="container">
+
+      <header>
+        <h1>Lista de presença</h1>
+        <div>
+          <strong>{user.name}</strong>
+          <img src={user.avatar} alt="" />
+        </div>
+      </header>
+
+      <input
+        type="text"
+        placeholder="Digite o nome..."
+        onChange={e => setStudentName(e.target.value)}
+      />
+
+      <button
+        type="button"
+        onClick={handleAddStudent}>
+        Adicionar
+      </button>
+
+      {
+        //percorrendo cada estudante da lista
+        students.map((student, index) =>
+          <Card
+            key={index}
+            name={student.name}
+            time={student.time} />
+        )
+      }
+    </div>
+  );
+}
+
+export default Home;
